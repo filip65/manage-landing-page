4086d9d560fec86a1b14bead01919c91996920f9
Filip Katusin <filip.katusin@gmail.com>
2021-07-20 11:22:14 +0200

    pridanie differences casti

--- src/App.js
+++ src/App.js
@@ -24,6 +24,57 @@ function App() {
             <button className="button">Get started</button>
           </div>
         </div>
+
+        <div className="differences-part">
+          <div className="differences-part__question">
+            <h2>What’s different about Manage?</h2>
+            <p>
+              Manage provides all the functionality your team needs, without the
+              complexity. Our software is tailor-made for modern digital product
+              teams.
+            </p>
+          </div>
+          <div className="differencies">
+            <ol className="differencies__items">
+              <li className="differencies__item">
+                <div className="header">
+                  <p className="number">01</p>
+                  <h3>Track company-wide progress</h3>
+                </div>
+                <p>
+                  See how your day-to-day tasks fit into the wider vision. Go
+                  from tracking progress at the milestone level all the way done
+                  to the smallest of details. Never lose sight of the bigger
+                  picture again.
+                </p>
+              </li>
+
+              <li className="differencies__item">
+                <div className="header">
+                  <p className="number">02</p>
+                  <h3>Advanced built-in reports</h3>
+                </div>
+                <p>
+                  Set internal delivery estimates and track progress toward
+                  company goals. Our customisable dashboard helps you build out
+                  the reports you need to keep key stakeholders informed.
+                </p>
+              </li>
+
+              <li className="differencies__item">
+                <div className="header">
+                  <p className="number">03</p>
+                  <h3>Everything you need in one place</h3>
+                </div>
+                <p>
+                  Stop jumping from one service to another to communicate, store
+                  files, track tasks and share documents. Manage offers an
+                  all-in-one team productivity solution.
+                </p>
+              </li>
+            </ol>
+          </div>
+        </div>
       </div>
     </div>
   );
